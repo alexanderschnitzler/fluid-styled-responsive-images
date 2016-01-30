605b9956b1e4128284e9825505557766f87bc3de
Alexander Schnitzler <git@alexanderschnitzler.de>
2016-01-30 17:08:23 +0100

    [BUGFIX] Add fluid_styled_content as dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,6 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => [
             'php' => '5.5.0-7.99.99',
             'typo3' => '7.5.0-7.99.99',
+            'fluid_styled_content' => '7.6.0',
         ],
         'conflicts' => [],
         'suggests' => [],
