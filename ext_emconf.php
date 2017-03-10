54c9771e1c046d04c3650838ee68f4addabd9223
Cedric Ziel <cedric@cedric-ziel.com>
2017-03-10 14:22:36 +0100

    Allow v8 in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.99.99',
-            'typo3' => '7.6.0-7.99.99',
-            'fluid_styled_content' => '7.6.0',
+            'typo3' => '7.6.0-8.6.99',
+            'fluid_styled_content' => '7.6.0-8.6.99',
         ],
         'conflicts' => [],
         'suggests' => [],
