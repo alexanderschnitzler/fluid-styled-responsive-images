df1e8c9f28eace5502d437ee178ea58ef4a3b85e
Cedric Ziel <cedric@cedric-ziel.com>
2017-03-14 14:49:38 +0100

    Raise PHP requirement and TYPO3 core constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email' => 'git@alexanderschnitzler.de',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.99.99',
-            'typo3' => '7.6.0-8.6.99',
-            'fluid_styled_content' => '7.6.0-8.6.99',
+            'php' => '7.0.0-7.99.99',
+            'typo3' => '8.6.0-8.7.99',
+            'fluid_styled_content' => '8.6.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
