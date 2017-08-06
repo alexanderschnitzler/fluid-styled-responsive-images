e034f460b22cee14530ba1bcc40cb32dd688239d
Alexander Schnitzler <git@alexanderschnitzler.de>
2017-08-06 10:26:04 +0200

    [TASK] Prepare master to become TYPO3 7 LTS branch

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email' => 'git@alexanderschnitzler.de',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.1.99',
-            'typo3' => '8.6.0-8.7.99',
-            'fluid_styled_content' => '8.6.0-8.7.99',
+            'php' => '5.5.0-7.1.99',
+            'typo3' => '7.6.13-7.6.99',
+            'fluid_styled_content' => '7.6.0-7.6.99',
         ],
         'conflicts' => [],
         'suggests' => [],
