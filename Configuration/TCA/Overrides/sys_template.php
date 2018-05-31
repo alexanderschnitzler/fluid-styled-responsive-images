fa7493516dc82e5582320ca30966c17c325b4d93
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2018-05-31 20:18:05 +0200

    Create sys_template.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,5 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('fluid_styled_responsive_images','Configuration/TypoScript','Fluid Styled Responsive Images');
