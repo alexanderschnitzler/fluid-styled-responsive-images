94660f3110eda4b04f45f0b98f73c93d2bd3e81f
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2018-06-01 08:46:36 +0200

    Delete ext_tables.php

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'fluid_styled_responsive_images',
-    'Configuration/TypoScript',
-    'Fluid Styled Responsive Images'
-);
