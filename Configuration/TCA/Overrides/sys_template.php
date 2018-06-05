752bb028add5213d540ace06ed210959313728af
Cedric Ziel <cedric@cedric-ziel.com>
2018-06-05 12:05:44 +0200

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-  'fluid_styled_responsive_images',
-  'Configuration/TypoScript',
-  'Fluid Styled Responsive Images'
+    'fluid_styled_responsive_images',
+    'Configuration/TypoScript',
+    'Fluid Styled Responsive Images'
 );
